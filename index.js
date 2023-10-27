12d55b6561b571d0cd5da368ec6f8f36621e880b
Tayler <taylerblevins@gmail.com>
2023-10-27 12:12:19 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>Tayler is the champion</h1>"
+
